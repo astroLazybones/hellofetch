77e32c31086e7b4e965e0ef4d8a6465dee68867c
Astro <148036040+astroLazybones@users.noreply.github.com>
2024-08-16 13:23:19 +1200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use compound_duration::format_dhms;
 //use std::time::Instant;
 use sysinfo::System; //Components, Disks, Networks, System};
 use whoami;
+use std::env;
 
 fn main() {
     // let cyan ="\x1b[0;36m";
@@ -20,11 +21,12 @@ fn main() {
     let namelen = format!("{}@{}", username, hostname).len();
     let possible_arch = System::cpu_arch().unwrap();
     let kernel = System::kernel_version().unwrap();
+    let osname = env::consts::OS;
 
     println!("\n{}\n{}", fullnaming, "-".repeat(namelen));
     println!("{}OS{}: {} {} {:?}", cybold, reset_col, name, ver, possible_arch);
     println!("{}Host{}: {}", cybold, reset_col, hostname);
-    println!("{}Kernel{}: {}", cybold, reset_col, kernel);
+    println!("{}Kernel{}: {} {}", cybold, reset_col, osname, kernel);
     println!("{}Uptime{}: {}\n", cybold, reset_col, format_dhms(System::uptime()));
 
     //let se = "Boot Time".bold().blue();
